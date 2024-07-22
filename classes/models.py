726cbe18f2299fd7674e5b6b7855e9be959633fa
brewchetta <chett.tiller@gmail.com>
2024-07-22 11:42:12 -0400

    objects, they relate!

--- classes/models.py
+++ classes/models.py
@@ -12,6 +12,21 @@ class Garage:
     def __repr__(self):
         return f"Garage(address={self.address})"
     
+    def cars(self):
+        # SELF IS A GARAGE
+        return [ car for car in Car.all_cars if car.garage == self ]
+
+        # my_cars = []
+
+        # for car in Car.all_cars:
+        #     if car.garage == self:
+        #         my_cars.append(car)
+
+        # return my_cars
+    
+    def my_self(self):
+        return self
+    
 
 # CAR ### BELONGS TO A GARAGE
 class Car:
@@ -19,15 +34,27 @@ class Car:
     # holds all the cars we've made
     all_cars = []
 
-    def __init__(self, make:str, model:str, license_plate:str):
+    def __init__(self, make:str, model:str, license_plate:str, garage:Garage):
         self.make = make
         self.model = model
         self.license_plate = license_plate
+        self.garage = garage
         Car.all_cars.append(self)
 
     def __repr__(self):
         return f"Car(make={self.make}, model={self.model}, license_plate={self.license_plate})"
-
+    
+    @property # GETTER
+    def garage(self):
+        return self._garage
+    
+    @garage.setter # SETTER
+    def garage(self, value):
+        # self IS THE CAR
+        if type(value) == Garage:
+            self._garage = value
+        else:
+            raise TypeError("garage must be of type Garage")
 
 
 
@@ -53,6 +80,13 @@ class Doctor:
     def __repr__(self):
         return f"Doctor(name={self.name}, specialty={self.specialty})"
     
+    def appointments(self):
+        return [ appt for appt in Appointment.all_appts if appt.doctor == self ]
+    
+    def patients(self):
+        return [ appt.patient for appt in Appointment.all_appts if appt.doctor == self ]
+
+    
 
 # PATIENT ###
 class Patient:
@@ -66,6 +100,12 @@ class Patient:
 
     def __repr__(self):
         return f"Patient(first_name={self.first_name}, last_name={self.last_name})"
+    
+    def appointments(self): # self is the patient
+        return [ appt for appt in Appointment.all_appts if appt.patient == self ]
+    
+    def doctors(self):
+        return list(set([ appt.doctor for appt in Appointment.all_appts if appt.patient == self ]))
 
 
 # APPT ###
@@ -73,13 +113,35 @@ class Appointment:
 
     all_appts = []
 
-    def __init__(self):
+    def __init__(self, doctor, patient):
+        self.doctor = doctor
+        self.patient = patient
         Appointment.all_appts.append(self)
 
     def __repr__(self):
         return f"Appointment(patient={self.patient.first_name}, doctor={self.doctor.name})"
     
+    @property
+    def doctor(self):
+        return self._doctor
     
+    @doctor.setter
+    def doctor(self, value):
+        if type(value) == Doctor:
+            self._doctor = value
+        else:
+            raise TypeError("doctor must be of type Doctor")
+
+    @property
+    def patient(self):
+        return self._patient
+    
+    @patient.setter
+    def patient(self, value):
+        if type(value) == Patient:
+            self._patient = value
+        else:
+            raise TypeError("patient must be of type Patient")
 
 
 ##############################
